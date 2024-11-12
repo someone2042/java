7e34a2b6715df69130a44dfc0b00e47e45831c2f
someone2042 <110247913+someone2042@users.noreply.github.com>
2024-11-12 08:45:56 +0100

    "Removed comments and updated matrix size from 4 to 3 in Cholesky Decomposition implementation."

--- TP SIT/GFG.java
+++ TP SIT/GFG.java
@@ -1,20 +1,13 @@
-// Java program to decompose
-// a matrix using Cholesky
-// Decomposition
 
 class GFG {
 
-	// static int MAX = 100;
 	static void Cholesky_Decomposition(int[][] matrix, int n) {
 		int[][] lower = new int[n][n];
 
-		// Decomposing a matrix
-		// into Lower Triangular
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
 				int sum = 0;
 
-				// summation for diagonals
 				if (j == i) {
 					for (int k = 0; k < j; k++)
 						sum += (int) Math.pow(lower[j][k], 2);
@@ -23,8 +16,6 @@ class GFG {
 
 				else {
 
-					// Evaluating L(i, j)
-					// using L(j, j)
 					for (int k = 0; k < j; k++)
 						sum += (lower[i][k] * lower[j][k]);
 					lower[i][j] = (matrix[i][j] - sum) / lower[j][j];
@@ -32,36 +23,27 @@ class GFG {
 			}
 		}
 
-		// Displaying Lower
-		// Triangular and its Transpose
 		System.out.println(" Lower Triangular\t Transpose");
 		for (int i = 0; i < n; i++) {
 
-			// Lower Triangular
 			for (int j = 0; j < n; j++)
 				System.out.print(lower[i][j] + "\t");
 			System.out.print("");
 
-			// Transpose of
-			// Lower Triangular
 			for (int j = 0; j < n; j++)
 				System.out.print(lower[j][i] + "\t");
 			System.out.println();
 		}
 	}
 
-	// Driver Code
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 3;
 		int[][] matrix = new int[][] {
-				{ 4, 12, 12, 16 },
-				{ 12, 37, 37, 43 },
-				{ 12, 37, 43, 98 },
-				{ 16, 43, 98, 10 }
+				{ 4, 12, 12 },
+				{ 12, 37, 37 },
+				{ 12, 37, 43 }
 		};
 
 		Cholesky_Decomposition(matrix, n);
 	}
 }
-
-// This code is contributed by mits
