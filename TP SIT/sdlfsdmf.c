086eb298dcc9d4713269f3de588e8c748888929e
someone2042 <110247913+someone2042@users.noreply.github.com>
2024-11-14 22:37:56 +0100

    more exercices

--- TP SIT/sdlfsdmf.c
+++ TP SIT/sdlfsdmf.c
@@ -2,41 +2,52 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-int S; // le resultat
-int r1 = 0;
-int r2 = 0;
-int factoriel(int n)
-{ // fonction pour calculer le factoriel
-    if (n == 0 || n == 1)
-        return 1;
-    else
-        return n * factoriel(n - 1);
-}
-void fonction1(void *arg)
+typedef struct
 {
-    int j = *(int *)arg;
-    r1 = factoriel(j);
-    pthread_exit(0);
-}
-void fonction2(void *arg)
+    int *array;
+    int taille;
+} Tableau;
+int c1 = 0;
+int c2 = 0;
+int S = 0;
+// fonction qui compte la premiere moitie des nbrs>=10
+void fct1(void *arg)
 {
-    int j = *(int *)arg;
-    r2 = factoriel(j);
-    pthread_exit(0);
+    Tableau tab = (Tableau)arg;
+    for (int i = 0; i < (tab->taille) / 2; i++)
+    {
+        if (tab->array[i] >= 10)
+            c1++;
+    }
+}
+// fonction qui compte la deuxieme moitie des nbrs>=10
+void fct2(void *arg)
+{
+    Tableau tab = (Tableau)arg;
+    for (int i = (tab->taille) / 2; i < tab->taille; i++)
+    {
+        if (tab->array[i] >= 10)
+            c2++;
+    }
 }
 
 int main()
 {
-    pthread_t th, ph;
-    int n = 3;
-    int p = 5;
-    // creation des threads
-    pthread_create(&th, NULL, fonction1, &n);
-    pthread_create(&ph, NULL, fonction2, &p);
-    // attendre que les threads finissent leur execution
+    Tableau A;
+    printf("entrer la taille du tableau:");
+    scanf("%d", &(A.taille));
+    A.array = malloc(sizeof(int) * A.taille);
+    for (int i = 0; i < A.taille; i++)
+    {
+        printf("entrer l element%d=", (i + 1));
+        scanf("%d", &(A.array[i]));
+    }
+    pthread_t th, th1;
+    pthread_create(&th, NULL, fct1, &A);
+    pthread_create(&th1, NULL, fct2, &A);
     pthread_join(th, NULL);
-    pthread_join(ph, NULL);
-    S = r1 + r2; // recuperation du resultat
-    printf(" la somme des deux factoriels est:%d\n", S);
+    pthread_join(th1, NULL);
+    S = c1 + c2;
+    printf(" le nombre d'entiers supérieur ou égale a 10 est:%d\n", S);
     return 0;
 }
